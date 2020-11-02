00eab94dc3edda2dedf2221e7ab800aa3b4fa8e1
andy-ang <andyangjunlong@outlook.sg>
2020-11-02 19:44:53 +0800

    Uploading new assignments

---
+++ 7_1_Brackets.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 18:21:46 2020
+
+@author: andya
+"""
+
+def solution(S):
+    new_S = S
+    check_brackets = ['[]','()','{}']
+    open_loop = 1
+    while(open_loop == 1):
+        
+        end = 0
+        found_closed = 0
+        if new_S == '':
+            break
+        
+        else:
+
+            if any(ext in new_S for ext in check_brackets):
+                new_S = new_S.replace('()', '')
+                new_S = new_S.replace('{}', '')
+                new_S = new_S.replace('[]', '')
+                found_closed = 1
+                      
+            else: found_closed = 0
+                                 
+        if found_closed == 0:
+            open_loop = 0
+             
+    if len(new_S) == 0:
+        answer = 1
+    else: answer = 0
+    
+    return answer
+
+
+
+solution('{[()()]}')
+solution('([)()]')   
+solution('({{({}[]{})}}[]{})')         
+            
